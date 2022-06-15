6fc6a032c2e6dd252134509265b27943bdfb0143
pramudyawid <dyaspramudyaz46@gmail.com>
2022-06-15 14:55:14 +0700

    Konsep Dasar

---
+++ application/views/home.php
@@ -0,0 +1,106 @@
+<div class="page-header">
+    <h1 class="bounceInDown">CINEMA 21</h1>
+</div>
+
+        <div class="jumbotron" style="width:100%;height:auto;margin-top:-20px">
+          <div class="container" style="padding-left:70px;padding-right:70px;">
+            <h2 class="satu" style="color:black;margin-bottom:90px;opacity:0">Cinema 21</h2>
+            <p class="satu" style="font-size:17px;text-align:justify;text-indent:30px opacity 0">Bioskop atau Cinema 21
+              adalah sebuah bioskop yang menampilkan film-film terbaik serta paling update.
+              Bioskop yang buka dari jam 10.00 sampai dengan jam 24.00 ini beralamat di Jl. Danau
+              Laut Tawar IV G5A-2 Sawojajar Kedungkandang Malang Kode Pos 65139. Bagi kalian yang ingin
+              menonton film bersama kerabat, teman, maupun pasangan, datang saja ke bioskop ini.
+              Dengan layar bioskop lebar serta kursi yang empuk, menambah kenyamanan anda dalam menonton film.
+            </p>
+          </div>
+        </div>
+
+          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="margin-top:-41px">
+          <div class="carousel-inner">
+            <div class="item active">
+              <img src="<?= base_url() ?>aset/Img/gp.jpg" style="width:100%;height:700px">
+            </div>
+            <div class="item">
+              <img src="<?= base_url() ?>aset/Img/jw.jpg" style="width:100%;height:700px">
+            </div>
+            <div class="item">
+              <img src="<?= base_url() ?>aset/Img/nns.jpg" style="width:100%;height:700px">
+            </div>
+          </div>
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+          </a>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+          </a>
+        </div>
+
+        <div class="jumbotron" style="background-color: rgb(93, 29, 14);width: 100%;height: auto">
+          <div class="container">
+            <h2 class="dua" style="opacity:0">Coming Soon</h2>
+            <div class="row dua" style="margin-top:60px;opacity:0">
+              <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                  <img src="<?= base_url() ?>aset/Img/bp.jpg" alt="...">
+                  <div class="caption">
+                    <h3 style="text-align:center">The Black Phone</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                  <img src="<?= base_url() ?>aset/Img/minion.jpg" alt="...">
+                  <div class="caption">
+                    <h3 style="text-align:center">Minions2 The Rise Of Gru</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                  <img src="<?= base_url() ?>aset/Img/r3.jpg" alt="...">
+                  <div class="caption">
+                    <h3 style="text-align:center">Ranah 3 Warna</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="container-fluid" style="padding-bottom: 50px">
+          <h2 style="color: black;font-size: 40px;text-align: center;margin-bottom: 70px">Owner's Profil</h2>
+          <div class="row">
+            <div class="col-md-8 empat" style="opacity:0">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Founder Cinema 21</h3>
+                </div>
+                <div class="panel-body">
+                  <p style="font-size:17px;text-align:justify;text-indent:30px">Kelompok kami,
+                    kami membangun bioskop ini yang kami namakan Cinema 21. Kenapa dinamakan begitu?
+                    Menurut penuturan 21 adalah angka yang sempurna.
+                  </p>
+                  <p style="font-size:17px;text-align:justify;text-indent:30px">
+                    Dibangunnya Cinema 21 adalah titik balik kesuksesan kami. kami berhasil meraih omset hingga
+                    ratusan juta perbulan. Kami hidup berkecukupan. kami berpesan pada semua orang, bahwa menjadi orang
+                    sukses tidak harus menunggu tua atau menunggu menyelesaikan pendidikan. Tetapi dapat
+                    dilakukan sewaktu-waktu yang penting ada niat dan kerja keras.
+                  </p>
+                  <p style="font-size:17px;text-align:justify;text-indent:30px">
+                    Karena menurut kami tanpa pengorbanan tiada kemenangan (No Sacrifice No Victory).
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 lima"  style="opacity:0">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Kelompok Kami</h3>
+                </div>
+                <div class="panel-body">
+                  <img src="<?= base_url() ?>aset/Img/diskusi.jpg">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
